5317e42832b62f266b383e949b3f3492d35ecc4b
csikbarni <csikbarni@gmail.com>
2017-11-16 17:44:07 +0100

    added gantt component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { GanttComponent } from './gantt/gantt.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GanttComponent
   ],
   imports: [
     BrowserModule
